6a149161b642996845304101f67c3111476c4674
Andrii Smahin <andrii.smahin@ekreative.com>
2019-08-14 11:50:14 +0300

    update room waiting

--- app/public/game/scripts/client.room.js
+++ app/public/game/scripts/client.room.js
@@ -10,17 +10,42 @@ socket.emit('authentication', {
 })
 
 socket.on('message', (data) => {
-  console.log('see console')
   Cookies.set('roomId', data.id) // id is roomId
-  console.log(data)
+  servicesCreateField(data)
 })
 
 socket.on('allPlayersInfo', (data) => {
-  console.log('allPlayersInfo see console')
-  console.log(data)
+  servicesCreateFields(data)
 })
 
-socket.on('letsBattle', () => {
-  alert('letsBattle')
-  location.replace('/game/battle')
-})
+// socket.on('letsBattle', () => {
+//   alert('letsBattle')
+//   location.replace('/game/battle')
+// })
+
+function servicesCreateFields(data) {
+  const divGrid = document.getElementById('grid')
+  divGrid.innerHTML = ''
+  data.forEach(element => {
+    const field = document.createElement('div')
+    field.innerHTML = `
+      <p>Name : <strong> ${element.name} </strong></p>
+      <p>Sessions : <strong> ${element.sessions}</strong></p>
+      <p>Wins : <strong> ${element.wins}</strong></p>
+      <p>LastPlayDate : <strong> ${new Date(element.wins).toLocaleString()}</strong></p>
+    `
+    divGrid.appendChild(field)
+  })
+}
+
+function servicesCreateField(data) {
+  const divGrid = document.getElementById('grid')
+  const field = document.createElement('div')
+  field.innerHTML = `
+      <p>Name : <strong> ${data.playerInfo.name} </strong></p>
+      <p>Sessions : <strong> ${data.playerInfo.sessions}</strong></p>
+      <p>Wins : <strong> ${data.playerInfo.wins}</strong></p>
+      <p>LastPlayDate : <strong> ${new Date(data.playerInfo.wins).toLocaleString()}</strong></p>
+    `
+  divGrid.appendChild(field)
+}
